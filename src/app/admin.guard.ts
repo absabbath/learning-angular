95d7b5546271a74ecc988b34c868630dff5be65e
Angel Martin Alejandro Santiago <angelmartin.isc@gmail.com>
2020-02-28 12:14:42 -0600

    login, logout, guard

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './core/services/auth.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.hasUser().pipe(
+      map(user => user !== null)
+    );
   }
-  
+
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -10,7 +10,20 @@ export class AuthService {
     private af: AngularFireAuth
   ) { }
 
-  createUser(email: string, password: string){
+  createUser(email: string, password: string) {
     return this.af.createUserWithEmailAndPassword(email, password);
   }
+
+  login(email: string, password: string) {
+    return this.af.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.af.signOut();
+  }
+
+  hasUser() {
+    return this.af.authState;
+  }
+
 }
