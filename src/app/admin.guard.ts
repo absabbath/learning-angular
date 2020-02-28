1017f87a3d6bf0b1147f297d6b9b49b11c7bb3a6
Angel Martin Alejandro Santiago <angelmartin.isc@gmail.com>
2020-02-28 12:20:18 -0600

    redirect if not has session

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './core/services/auth.service';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.hasUser().pipe(
-      map(user => user !== null)
+      map(user => user !== null),
+      tap(hasSession => {
+        if (!hasSession) {
+          this.router.navigate(['/auth/login']);
+        }
+      })
     );
   }
 
